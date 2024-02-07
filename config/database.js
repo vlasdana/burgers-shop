e13031b56cf397cbddc37452c91dacc96f972de4
Dana Vlas <dana-monica.vlas@edu.fh-joanneum.at>
2024-02-08 00:58:17 +0100

    Add checklist.

--- config/database.js
+++ config/database.js
@@ -3,6 +3,7 @@ import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 dotenv.config();
 
+//create connection with database
 const pool = mysql.createPool({
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "admin",
